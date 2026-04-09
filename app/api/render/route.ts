fc3371b696ba519113acb09a58f70510f917d1e9
illiakovalenko <illia.kovalenko@sitecore.com>
2026-04-09 09:51:01 +0300

    Remove unused variables

--- app/api/render/route.ts
+++ app/api/render/route.ts
@@ -1,7 +1,7 @@
-import { cookies as nextCookies, draftMode } from "next/headers";
+import { draftMode } from "next/headers";
 import { redirect } from "next/navigation";
 
-export async function GET(request: Request) {
+export async function GET() {
   const draft = await draftMode();
 
   draft.enable();
